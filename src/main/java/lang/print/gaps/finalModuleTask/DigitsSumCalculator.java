b954b66f555bd9f8dff882198dd1765fc91b57dd
Artyom <artvit12@gmail.com>
2022-08-06 13:07:32 +0600

    Solution

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+
+        int thousands = number / 1000;
+        int hundreds = number % 1000 / 100;
+        int decimals = number % 1000 % 100 /10;
+        int digit = number % 1000 % 100 % 10;
+        System.out.println(thousands + hundreds + decimals + digit);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,9 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int hundreds = number / 100;
+        int decimals = (number - hundreds * 100) / 10;
+        int digit = number - hundreds * 100 - decimals * 10;
+        System.out.println(digit + "" + decimals + "" + hundreds);
     }
 }
